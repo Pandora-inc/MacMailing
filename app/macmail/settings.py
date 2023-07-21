3122fb8d0e01bfa7c43be7867c55590d6d4af453
Ivan K Berlot <ivanberlot@gmail.com>
2023-07-21 14:00:51 -0300

    a

--- app/macmail/settings.py
+++ app/macmail/settings.py
@@ -144,29 +144,29 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
 # ADMIN_SITE = 'reportes.admin.my_admin_site'
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    # 'filters': {
-    #     'require_debug_false': {
-    #         '()': 'django.utils.log.RequireDebugFalse'
-    #     }
-    # },
-    # 'handlers': {
-    #     'mail_admins': {
-    #         'level': 'ERROR',
-    #         'filters': ['require_debug_false'],
-    #         'class': 'django.utils.log.AdminEmailHandler'
-    #     }
-    # },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
+# LOGGING = {
+#     'version': 1,
+#     'disable_existing_loggers': False,
+#     'filters': {
+#         'require_debug_false': {
+#             '()': 'django.utils.log.RequireDebugFalse'
+#         }
+#     },
+#     'handlers': {
+#         'mail_admins': {
+#             'level': 'ERROR',
+#             'filters': ['require_debug_false'],
+#             'class': 'django.utils.log.AdminEmailHandler'
+#         }
+#     },
+#     'loggers': {
+#         'django.request': {
+#             'handlers': ['mail_admins'],
+#             'level': 'ERROR',
+#             'propagate': True,
+#         },
+#     }
+# }
 
 
 # ADMIN_REORDER = (
