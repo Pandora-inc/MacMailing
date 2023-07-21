965ee621177b339dee0b96e9169d0fdd13212379
Ivan K Berlot <ivanberlot@gmail.com>
2023-07-21 13:51:31 -0300

    a

--- app/macmail/settings.py
+++ app/macmail/settings.py
@@ -147,18 +147,18 @@ SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
+    # 'filters': {
+    #     'require_debug_false': {
+    #         '()': 'django.utils.log.RequireDebugFalse'
+    #     }
+    # },
+    # 'handlers': {
+    #     'mail_admins': {
+    #         'level': 'ERROR',
+    #         'filters': ['require_debug_false'],
+    #         'class': 'django.utils.log.AdminEmailHandler'
+    #     }
+    # },
     'loggers': {
         'django.request': {
             'handlers': ['mail_admins'],
