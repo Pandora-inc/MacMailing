31229e5eff78a0a0269e1c1dc54d3940c4eaca40
Ivan K Berlot <ivanberlot@gmail.com>
2023-10-05 20:27:19 -0300

    test

--- app/reportes/admin.py
+++ app/reportes/admin.py
@@ -248,6 +248,8 @@ class MailAdmin(admin.ModelAdmin):
     readonly_fields = ('last_send', 'send_number', 'created')
 
     actions = [prepare_to_send]
+    
+    list_display_links = None
 
     def get_queryset(self, request):
         # Obtener el queryset base
@@ -269,11 +271,6 @@ class MailAdmin(admin.ModelAdmin):
                                            'template_group', 'reminder_days', 'use_template',) 
         return self.readonly_fields
     
-    def actions_on_top(self, request, queryset):
-        # Desactiva las acciones en la parte superior cuando 'fin' es True
-        if queryset.filter(status_response=True).exists():
-            return None
-        return super().actions_on_top(request, queryset)
 
     def proximo(self, obj):
         '''
