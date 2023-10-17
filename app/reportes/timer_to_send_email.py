cd6c577acc6bff1b234cbd95c254b072344b977f
Ivan K Berlot <ivanberlot@gmail.com>
2023-10-17 11:14:35 -0300

    a

--- app/reportes/timer_to_send_email.py
+++ app/reportes/timer_to_send_email.py
@@ -4,7 +4,7 @@ import logging
 def timer_to_send_email(request):
    try:
       api_host = "http://macmailling.eastus.cloudapp.azure.com:8000"
-      api_path = "send_next_email/"
+      api_path = "/send_next_email/"
       conn = http.client.HTTPSConnection(api_host)
       print(conn)
       conn.request("POST", api_path)
@@ -15,6 +15,6 @@ def timer_to_send_email(request):
          logging.info('Error en la solicitud POST: response.status_code')
          logging.info(response.status)
    except Exception as e:
-      logging.info('Error en la solicitud POST: response.status_code')
-      logging.info(e)
+      print('Error en la solicitud POST: response.status_code')
+      print(e)
    logging.info('Python timer trigger function executed.')
