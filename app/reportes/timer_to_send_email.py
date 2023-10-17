63459b932e38dce2c873653279529108ec1afc44
Ivan K Berlot <ivanberlot@gmail.com>
2023-10-17 11:03:00 -0300

    a

---
+++ app/reportes/timer_to_send_email.py
@@ -0,0 +1,15 @@
+import http.client
+import logging
+
+def timer_to_send_email():
+   api_host = "http://macmailling.eastus.cloudapp.azure.com:8000"
+   api_path = "send_next_email/"
+   conn = http.client.HTTPSConnection(api_host)
+   conn.request("POST", api_path)
+   response = conn.getresponse()
+   if response.status == 200 or response.status == 208:
+      logging.info('Solicitud POST exitosa')
+   else:
+      logging.info('Error en la solicitud POST: response.status_code')
+      logging.info(response.status)
+   logging.info('Python timer trigger function executed.')
