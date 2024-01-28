e002aa266b3528aecb1fdc23e4c3131e96d5d3db
Ivan K Berlot <ivanberlot@gmail.com>
2024-01-28 16:56:44 -0300

    a

--- app/reportes/tests/tests.py
+++ app/reportes/tests/tests.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from django.test import TestCase
-from calendarapp.models import Event
-from reportes.actions import crear_evento
-from reportes.models import Mail
+from prueba import MockObject
+# from reportes.actions import crear_evento
+# from reportes.models import Mail
 
 
 class MailTestCase(TestCase):
@@ -10,15 +10,17 @@ class MailTestCase(TestCase):
 
     def test_creates_new_event(self):
         """ Test that a new event is created """
+        mock_object = MockObject(1)
+        assert mock_object.get_value() == 1
         # Arrange
-        mail = Mail()
-        mail.send_number = 1
-        mail.subject = "Test Subject"
-        mail.last_send = datetime.now()
-        mail.reminder_days = 7
+        # mail = Mail()
+        # mail.send_number = 1
+        # mail.subject = "Test Subject"
+        # mail.last_send = datetime.now()
+        # mail.reminder_days = 7
 
-        # Act
-        crear_evento(mail)
+        # # Act
+        # crear_evento(mail)
 
         # Assert
-        assert Event.objects.filter(title="1 - Test Subject").exists()
+        # assert Event.objects.filter(title="1 - Test Subject").exists()
