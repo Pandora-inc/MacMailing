710a7c69605c10a852ade469871ab607b4df1b1a
Ivan K Berlot <ivanberlot@gmail.com>
2024-03-07 20:58:00 -0300

    correct hearbeat problem

--- heartbeat.sh
+++ heartbeat.sh
@@ -2,12 +2,12 @@
 
 server=$(netstat -ano | grep 8000)
 
-if [ $server ]
+if [ "${server}" ]
 then
         echo true > logs/status.log
         echo $server >> logs/status.log
 else
         echo false > logs/status.log
         echo $(date +"%Y-%m-%d %H:%M") >> ~/logs/cron.log
-        python3 ~/MacMailing/app/manage.py runserver 0.0.0.0:8000>logs/$(date +"%Y-%m-%d").log &
+        python3 ~/MacMailing/app/manage.py runserver 0.0.0.0:8000>>logs/$(date +"%Y-%m-%d").log &
 fi
