19529dac995c31740b3d6a2fce09072e64d9c032
Ivan K Berlot <ivanberlot@gmail.com>
2024-03-07 20:11:49 -0300

    Correct error

--- inicio.sh
+++ inicio.sh
@@ -3,4 +3,7 @@ nohup python3 ~/MacMailing/app/manage.py runserver 0.0.0.0:8000 &
 
 
 crontab -e
-@reboot nohup python3 ~/projets/MacMailing/app/manage.py runserver 0.0.0.0:8000>app.log &
+*/5 * * * * sh ~/MacMailing/heartbeat.sh
+@reboot nohup python3 ~/projets/MacMailing/app/manage.py runserver 0.0.0.0:8000>logs/$(date +"%Y-%m-%d").log &
+
+
