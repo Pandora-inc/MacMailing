302eb1f79848f717cc41612d7313129f200f1c11
Ivan K Berlot <ivanberlot@gmail.com>
2024-06-23 00:17:41 -0300

    Create APIS

--- reportes/apis.py
+++ reportes/apis.py
@@ -4,6 +4,8 @@ import json
 import requests
 import json
 
+from reportes.models import Clientes
+from reportes.serializers import ClientesSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -18,9 +20,7 @@ class MyAPIView(APIView):
     def post(self, request, *args, **kwargs):
         """ Método que recibe un JSON y lo imprime en la consola """
         data = request.data
-        content = literal_eval(data['_content'])
-
-        changed_data_id = content['data[FIELDS][ID]']
+        changed_data_id = data['data[FIELDS][ID]']
 
         base_url = "https://b24-tgz7td.bitrix24.es/rest/1/b5a32slysrg26vwr/crm.contact.get.json?ID="
         url = base_url + changed_data_id
@@ -28,15 +28,24 @@ class MyAPIView(APIView):
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-
         response = requests.request("POST", url, headers=headers)
-        print(response)
-        print(response.text)
-        # https://b24-tgz7td.bitrix24.es/rest/1/b5a32slysrg26vwr/crm.contact.get.json?ID=2
-        # serializer = MySerializer(data=request.data)
-        return Response(response.content, status=status.HTTP_200_OK)
-        # if serializer.is_valid():
-            # print(json.dumps(serializer.validated_data, indent=4))  # Imprime el JSON en la consola
 
-            # return Response(serializer.data, status=status.HTTP_200_OK)
-        # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        data = json.loads(response.content)
+        if data.get('result'):
+            data = data.get('result')
+
+        cliente_id = data.get('cliente_id', data.get('ID'))
+
+        if not cliente_id:
+            return Response({"error": "cliente_id is required"}, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            cliente = Clientes.objects.get(cliente_id=cliente_id)
+        except Clientes.DoesNotExist:
+            return Response({"error": "Cliente not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        serializer = ClientesSerializer(cliente, data=data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
