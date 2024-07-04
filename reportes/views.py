1f6218973fad1feb0b603aff92ff33710c846c23
Ivan K Berlot <ivanberlot@gmail.com>
2024-07-04 13:33:23 -0300

    Add pipeline corrections

--- reportes/views.py
+++ reportes/views.py
@@ -4,7 +4,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import status
-from django.db import connection
 from django.shortcuts import render
 
 from .forms import MailForm
